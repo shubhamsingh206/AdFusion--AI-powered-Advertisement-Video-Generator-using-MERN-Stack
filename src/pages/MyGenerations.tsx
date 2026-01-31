d0aa8600facbb9f402ea875dfe72bd548f2e7acd
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-01-31 21:09:37 +0530

    Add new things

--- src/pages/MyGenerations.tsx
+++ src/pages/MyGenerations.tsx
@@ -16,9 +16,9 @@ const MyGenerations = () => {
     fetchMyGenerations();
   }, []);
   return loading ? (
-  <di className="flex items-center justify-center min-h-screen">
+  <div className="flex items-center justify-center min-h-screen">
       <Loader2Icon className="size-7 animate-spin text-indigo-400" />
-    </di>
+    </div>
   )
 }
 export default MyGenerations;
