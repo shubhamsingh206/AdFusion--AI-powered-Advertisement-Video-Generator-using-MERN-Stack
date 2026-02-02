8e41b3903c971077648315687f42091a2810b50f
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-02-02 11:43:45 +0530

    Add new things

--- src/pages/MyGenerations.tsx
+++ src/pages/MyGenerations.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import type { Project } from "../Types";
 import { dummyGenerations } from "../assets/assets";
 import { Loader2Icon } from "lucide-react";
+import ProjectCard from "../components/ProjectCard";
 
 const MyGenerations = () => {
   const [generations, setGenerations] = useState<Project[]>([]);
@@ -16,9 +17,33 @@ const MyGenerations = () => {
     fetchMyGenerations();
   }, []);
   return loading ? (
-  <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center min-h-screen">
       <Loader2Icon className="size-7 animate-spin text-indigo-400" />
     </div>
-  )
-}
+  ) : (
+    <div className="min-h-screen text-white p-6 md:p-12 my-28">
+      <div className="max-w-6xl mx-auto">
+        <header className="mb-12">
+          <h1 className="text-3xl md:text-4xl font-semibold mb-4">
+            My Generations
+          </h1>
+          <p className="text-gray-400">
+            View and manage your AI-generated content
+          </p>
+        </header>
+
+        {/* generations list */}
+        <div className="columns-1 sm: columns-2 lg: columns-3 gap-4">
+          {generations.map((gen) => (
+            <ProjectCard
+              key={gen.id}
+              gen={gen}
+              setGenerations={setGenerations}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 export default MyGenerations;
