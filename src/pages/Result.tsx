47e1e7a2f52e5ddbf8c92de38e23ba7a6aa85fc3
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-02-06 12:14:12 +0530

    Edit files

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import type { Project } from "../Types";
 import { dummyGenerations } from "../assets/assets";
-import { Link, Loader2Icon, RefreshCwIcon } from "lucide-react";
+import { Loader2Icon, RefreshCwIcon } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Result = () => {
   const [project, setProjectData] = useState<Project>({} as Project)
   const [loading, setLoading] = useState(true)
-  const [isGenerating, setIsGeneratin] = useState(false)
+  const [isGenerating, setIsGenerating] = useState(false)
   
   const fetchProjectData = async ()=>{
     setTimeout (()=>{
@@ -20,20 +21,22 @@ const Result = () => {
     fetchProjectData()  
   }, [])
   return loading ? (
-  <div className="h-screen w-full flex items-center justify-center">
-    <Loader2Icon className='animate-spin text-indigo-500 size-9'/>
+    <div className="h-screen w-full flex items-center justify-center">
+     <Loader2Icon className='animate-spin text-indigo-500 size-9'/>
     </div>
   ) : (
   <div className="min-h-screen text-white p-6 md:p-12 mt-20">
     <div className="max-w-6xl mx-auto">
       <header className="flex justify-between items-center mb-8">
         <h1 className="text-2xl md:text-3xl font-medium">Generation Results</h1>
-        <Link to="/generate" className="btn-secondary flex items-center 
+        <Link to="/generate" className="btn-secondary text-sm flex items-center 
         gap-2">
-          <RefreshCwIcon className="w-4 h-4" />
-          <p>New Generation</p>
+          <RefreshCwIcon className="w-4  h-4"/>
+          <p className="max-sm:hidden">New Generation</p>
           </Link>
         </header>
+        {/* grid layout */}
+        <div className="grid lg:grid-cols-3 gap-8"></div>
       </div>
     </div>
   );
