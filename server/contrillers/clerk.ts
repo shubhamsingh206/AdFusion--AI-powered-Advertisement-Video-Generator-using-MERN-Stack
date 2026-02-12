03d3eae2368759da7c8652777168d3413a7714e0
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-02-12 22:47:35 +0530

    Update clerk.ts file

--- server/contrillers/clerk.ts
+++ server/contrillers/clerk.ts
@@ -42,6 +42,29 @@ const clerkWebhooks = async (req: Request, res: Response) =>{
                 break;
             }
 
+            case "paymentAttempt.updated": {
+                if((data.charge_type === "recurring" || data.charge_type ===
+                "checkout") && data.status === "paid"){
+                    const credits = {pro: 80, premium: 240,}
+                    const clerkUserId = data?.payer?.user_id;
+                    const planId: keyof typeof credits = data?.subscription_items?.
+                    [0]?.plan?.slug;
+
+                    if(planId !== "pro" && planId !== "premium"){
+                        return res.status(400). json({message: "Invalid plan"})
+                    }
+                    console.log(planId)
+
+                    await prisma.user.update({
+                        where: {id: clerkUserId,},
+                        data: {
+                            credits: {increment: credits[planId]}
+                        }
+                    })
+                }
+                break;
+            }
+
             default:
                 break;
         }
