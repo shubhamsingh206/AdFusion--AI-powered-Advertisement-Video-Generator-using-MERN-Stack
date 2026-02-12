cd85f91e5623775dde4996baa70c261ce2be3dfe
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-02-12 22:30:47 +0530

    Update clerk.ts file

--- server/contrillers/clerk.ts
+++ server/contrillers/clerk.ts
@@ -1,10 +1,51 @@
 import { Request, Response } from 'express';
 import { verifyWebhook } from '@clerk/express/webhooks'
+import { prisma } from '../configs/prisma.js';
 
 const clerkWebhooks = async (req: Request, res: Response) =>{
     try {
         const evt: any = await verifyWebhook(req)
+
+        //Getting Data from request
+        const{ data, type} = evt;
+
+        //Switch Cases for different Events
+        switch (type){
+            case "user.created": {
+                await prisma.user.create({
+                    data: {
+                        id: data.id,
+                        email: data ?. email_email_addresses[0] ?. email_address,
+                        name: data ?. first_name + " " + data ?. last_name,
+                        image: data ?. image_url,
+                    }
+                })
+                break;
+            }
+
+             case "user.updated": {
+                await prisma.user.update({
+                    where: {
+                        id: data.id
+                    },
+                    data: {
+                        email: data ?. email_email_addresses[0] ?. email_address,
+                        name: data ?. first_name + " " + data ?. last_name,
+                        image: data ?. image_url,
+                    }
+                })
+                break;
+            }
+            
+             case "user.deleted": {
+                await prisma.user.delete({where: {id: data.id}})
+                break;
+            }
+
+            default:
+                break;
+        }
     } catch (error) {
-        
+
     }
 }
