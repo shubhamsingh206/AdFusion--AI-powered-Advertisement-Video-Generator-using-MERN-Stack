4862a0919fdd249f262543204655bf106f4d9aef
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-02-12 21:41:32 +0530

    Update server configs, prisma setup, and middleware

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,16 @@
 import express, { Request, Response } from 'express';
 import cors from'cors'
 import 'dotenv/config'
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
 //Middleware
 app.use(cors())
 app.use(express.json())
+app.use(clerkMiddleware())
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
