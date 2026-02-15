45aabb8f9ca24b913f907b28be3e7a75a86a9b0a
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-02-15 16:28:28 +0530

    feat: implement user controller and user routes

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from 'express';
 import * as Sentry from "@sentry/node"
+import { prisma } from '../configs/prisma.js';
 
 //Get User Credits
-export const getUserCredits = async (req: Request, res: Response) =>{
+export const getUserCredits = async (req: Request, res: Response) => {
     try{
+
+        const {userId} = req.auth();
+        if(!userId) {return res.status(401).json({message: 'Unauthorized'})}
+        const user = await prisma.user.findUnique({
+            where: {id: userId}
+        })
+        res.json({credits: user ?. credits})
 
     } catch (error: any) {
             Sentry.captureException(error)
