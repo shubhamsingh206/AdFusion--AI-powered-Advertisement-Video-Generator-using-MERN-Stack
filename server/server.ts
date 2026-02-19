736c29cede86c8e1648c192ea29a4ecc162a02a3
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-02-19 21:51:11 +0530

    Backend setup: added auth middleware, Prisma config, project & user controllers with routes

--- server/server.ts
+++ server/server.ts
@@ -6,6 +6,7 @@ import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from './controllers/clerk.js';
 import * as Sentry from "@sentry/node"
 import userRouter from "./routes/userRoutes.js";
+import projectRouter from "./routes/projectRoutes.js";
 
 const app = express();
 
@@ -28,6 +29,7 @@ app.get("/debug-sentry", function mainHandler(req, res) {
 });
 
 app.use('/api/user', userRouter)
+app.use('/api/project', projectRouter)
 
 // The error handler must be registered before any other error middleware and after all controllers
 Sentry.setupExpressErrorHandler(app);
