0afcdaa19db7894692ee47d85cc5cefdd9b6e24b
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-02-20 10:16:55 +0530

    refactor(server): update server.ts

--- server/server.ts
+++ server/server.ts
@@ -31,3 +31,11 @@ app.get("/debug-sentry", function mainHandler(req, res) {
 app.use('/api/user', userRouter)
 app.use('/api/project', projectRouter)
 
+// The error handler must be registered before any other error middleware and after all controllers
+Sentry.setupExpressErrorHandler(app);
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+
+    
+});
