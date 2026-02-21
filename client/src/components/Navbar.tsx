c834c9918ee5212d7a1b1e5f2974d9c8ba1b6c1e
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-02-21 21:54:59 +0530

    feat(client): refine UI components and enhance navbar structure

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,9 +2,11 @@ import { DollarSignIcon, FolderEditIcon, GalleryHorizontalEnd, MenuIcon, Sparkle
 import { GhostButton, PrimaryButton } from "./Buttons";
 import { useState } from "react";
 import { motion } from "framer-motion";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
-import { useClerk, useUser, UserButton } from "@clerk/clerk-react";
+import { useClerk, useUser, UserButton, useAuth } from "@clerk/clerk-react";
+import toast from "react-hot-toast";
+import api from "../configs/axios";
 
 export default function Navbar() {
   const navigate = useNavigate()
@@ -13,12 +15,29 @@ export default function Navbar() {
 
   const [isOpen, setIsOpen] = useState(false);
 
+  const [Credits, setCredits] = useState(0);
+  const { pathname } =  useLocation()
+  const { getToken } = useAuth()
+
   const navLinks = [
     { name: "Home", href: "/#" },
     { name: "Create", href: "/generate" },
     { name: "Community", href: "/community" },
     { name: "Plans", href: "/plans" },
   ];
+
+  const getUserCredits = async ()=>{
+    try {
+      const token = await getToken()
+      const {data} = await api.get('/api/user/credits', {headers:
+        {Authorization: `Bearer ${token}`}})
+        setCredits(data.credits)
+      } // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      catch (error: any) {
+        toast.error(error?.response?.data?.message || error.message)
+        console.log(error);
+      }
+    }
 
   return (
     <motion.nav
