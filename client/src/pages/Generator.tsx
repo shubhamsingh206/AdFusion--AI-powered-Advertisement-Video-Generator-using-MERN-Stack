a8ccc95c27f43c54244f27ac7fe0a372fd8516a5
shubhamsingh206 <shubhamsingh82735@gmail.com>
2026-02-21 23:14:53 +0530

    feat(client): feat: add core application pages and routing structure

--- client/src/pages/Generator.tsx
+++ client/src/pages/Generator.tsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 import Title from "../components/Title";
 import UploadZone from "../components/UploadZone";
-import {
-  Loader2Icon,
-  RectangleHorizontalIcon,
-  RectangleVerticalIcon,
-  Wand2Icon,
-} from "lucide-react";
+import { Loader2Icon, RectangleHorizontalIcon, RectangleVerticalIcon, Wand2Icon, } from "lucide-react";
 import { PrimaryButton } from "../components/Buttons";
+import { useAuth, useUser } from "@clerk/clerk-react";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import api from "../configs/axios";
 
 const Generator = () => {
+
+  const {user} = useUser()
+  const {getToken} = useAuth()
+  const navigate = useNavigate()  
+
   const [name, setName] = useState("");
   const [productName, setProductName] = useState("");
   const [productDescription, setProductDescription] = useState("");
@@ -30,6 +34,35 @@ const Generator = () => {
   };
   const handleGenerate = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if(!user) return toast('Please login to generate')
+
+    if(!productImage || !modelImage || !name || !productName || !aspectRatio)
+    return toast('Please fill all the required fields')
+
+     try {
+      setIsGenerating(true);
+      const formData = new FormData();
+      formData.append('name', name)
+      formData. append('productName', productName)
+      formData.append('productDescription', productDescription)
+      formData.append('userPrompt', userPrompt)
+      formData.append('aspectRatio', aspectRatio)
+      formData.append('images', productImage)
+      formData.append('images', modelImage)
+
+      const token = await getToken()
+
+      const { data } = await api.post('/api/project/create', formData, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      
+      toast.success(data.message)
+      navigate('/result/' + data.projectId)
+     } // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      catch (error: any) {
+      setIsGenerating(false);
+      toast.error(error?.response?.data?.message || error.message)
+     }
   };
 
   return (
